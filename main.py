689c2dd5abe66926533c9480a5469e927878199d
Carlil Jean <jaedagk@live.com>
2022-06-10 03:20:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+import art
+
+while True:
+  print(art.logo)
+  name = input("Please enter your full name.")
+  price = input("Please enter bid price.")
+  bid = {}
+  for a in ["name", "price"]:
+    bid[a] = eval(a)
+    while True:
+      answer = str(input('Run again? (y/n)?: '))
+    if answer in ('y', 'n'):
+      break
+    print("invalid input.")
+    if answer == 'y':
+      continue
+    else: 
+      print("Goodbye.")
+      break
+    
+
