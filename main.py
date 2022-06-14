79de8ca3c1a9e47091d559b02bdedea4d1e338af
Carlil Jean <jaedagk@live.com>
2022-06-14 05:42:31 +0000

    Created functions.

--- main.py
+++ main.py
@@ -2,13 +2,35 @@ from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 import art
 
-while True:
-  print(art.logo)
+bid = {}
+
+def GetBid():
   name = input("Please enter your full name: ")
   price = input("Please enter bid price: ")
-  bid = {}
   for a in ["name", "price"]:
     bid[a] = eval(a)
+
+def SeeIfThereAreOtherBidders():
+  question = input("Are there any other bidders?")
+    if question in ('y', 'n'):
+      break
+  print("invalid input.")
+  if answer == 'y':
+    GetBid()
+    continue
+  else: 
+    FindHighestBidder()
+    break
+
+def FindHighestBidder():
+    highestBid = max(bid, key=bid.get)
+    print("The highest bidder is:", highestBid)
+  
+
+
+while True:
+  print(art.logo)
+  GetBid()
 
   while True:
     answer = str(input('Run again? (y/n)?: '))
